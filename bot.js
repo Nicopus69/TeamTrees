8059fd62f6678592d6c77bb42ca4611dca78adc1
Nicopus69 <Nicopus69>
2019-11-30 16:29:03 +0000

    ping command fixed

--- bot.js
+++ bot.js
@@ -5,26 +5,26 @@ var activity =  "planting trees!"
 
 client.on("ready", () => {
     console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-
-    client.user.setActivity(`${activity}`);
-    client.on("guildCreate", guild => {
-        console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-        client.user.setActivity(`${activity}`);
-    });
-
-
-
-    client.on("message", async message => {
-        if(message.author.bot) return;
-        if(message.content.indexOf(config.prefix) !== 0) return;{
-            const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-            const command = args.shift().toLowerCase();
-
-            if(command === "ping") {
-                const m = await message.channel.send("Ping?");
-                m.edit(`🏓Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-            }
-
-        }
+    client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  });
+  
+  client.on("guildCreate", guild => {
+    console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+    client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  });
+  
+  client.on("guildDelete", guild => {
+    console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+    client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  });
+  
+  
+  client.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.content.indexOf(config.prefix) !== 0) return;
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    if(command === "ping") {
+      const m = await message.channel.send("Ping?");
+      m.edit(`🏓Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
     }
-});
